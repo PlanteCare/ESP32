9e716e55c27ee2979b54ba1c19fc4d0e56b3b771
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2025-03-14 00:33:18 -0400

    Implémentation de la connexion wifi et mqtt

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,33 @@
 #include <Arduino.h>
+#include "MyMqtt.h"
+#include "MyWifi.h"
 
-// put function declarations here:
-int myFunction(int, int);
+
+#define SERIAL_SPEED 9600
+#define MQTT_SERVER "lyeshamrani.com"
+#define MQTT_PORT 1883
+#define WIFI_SSID "GIGACHAD"
+#define WIFI_PASSWORD "BigNoobs999"
+#define MQTT_TOPIC "plantecare/message"
+#define LOOP_DELAY 60000
+
+MyMqtt *myMqtt = nullptr;
+MyWifi *myWifi = nullptr;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(SERIAL_SPEED);
+  myWifi = new MyWifi();
+  myWifi->init(WIFI_SSID, WIFI_PASSWORD);
+  myWifi->connect();
+  myMqtt = new MyMqtt();
+  myMqtt->init(MQTT_SERVER, MQTT_PORT);
+  myMqtt->connect();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  myMqtt->publish(MQTT_TOPIC, "Hello from ESP32");
+  myMqtt->loop();
+  delay(LOOP_DELAY);  
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
